59706db399e4494c51c813605c6e1e4d74b08b57
pancomputationalist <15155173+bearbytes@users.noreply.github.com>
2018-09-01 15:51:28 +0200

    createId

--- src/state/spawnUnit.ts
+++ src/state/spawnUnit.ts
@@ -1,9 +1,10 @@
 import { IGameState, IUnit } from '../types'
 import { ClickOnTile } from '../actions'
 import { addUnit, updateTile } from './helpers'
+import { createId } from '../lib/createId'
 
 export function spawnUnit(s: IGameState, a: ClickOnTile): IGameState {
-  const unitId = 'blablabla'
+  const unitId = createId('unit')
   const unit: IUnit = { unitId }
   s = addUnit(s, unit)
   s = updateTile(s, a.tileId, { unitId })
