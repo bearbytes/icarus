a2077772aa2775ed104e8fe8f86dc4ef4028cbdf
pancomputationalist <15155173+bearbytes@users.noreply.github.com>
2018-09-08 12:38:20 +0200

    styling

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,23 +3,34 @@ import styled from 'styled-components'
 
 interface ButtonProps {
   text: string
-  down?: boolean
   onClick: () => void
+
+  down?: boolean
+  disabled?: boolean
 }
 
 export default function Button(props: ButtonProps) {
   return (
-    <StyledButton down={props.down || false} onClick={props.onClick}>
+    <StyledButton
+      onClick={() => {
+        if (!props.disabled) props.onClick()
+      }}
+      down={props.down || false}
+      disabled={props.disabled || false}
+    >
       {props.text}
     </StyledButton>
   )
 }
 
-const StyledButton = styled.div<{ down: boolean }>`
+const StyledButton = styled.div<{ down: boolean; disabled: boolean }>`
   font-size: 120%;
   background-color: ${p => (p.down ? '#ccc' : '#888')};
   border: 2px transparent;
   border-radius: 8px;
   padding: 10px;
   margin: 2px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
--- src/components/EndTurnButton.tsx
+++ src/components/EndTurnButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import styled from 'styled-components'
 import { withClientStateAndDispatch } from './hoc/withClientState'
+import Button from './Button'
 
 export default function EndTurnButton() {
   return withClientStateAndDispatch(
@@ -8,19 +8,11 @@ export default function EndTurnButton() {
       disabled: s.game.activePlayerId != s.ui.localPlayerId,
     }),
     s => (
-      <StyledButton
+      <Button
+        text={'End Turn'}
         disabled={s.disabled}
         onClick={() => s.dispatch({ type: 'ClickOnEndTurn' })}
-      >
-        End Turn
-      </StyledButton>
+      />
     ),
   )
 }
-
-const StyledButton = styled.button`
-  background-color: #ddd;
-  font-size: 2em;
-  border: 1px solid #888;
-  border-radius: 5px;
-`
